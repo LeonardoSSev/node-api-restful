d83c8178e32f9aa93717c52119bf44784153bf2d
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-31 20:55:23 -0300

    Uses port from environment variable

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',  (req, res) => {
    res.send('Hello World');
 });
 
-app.listen(3000, () => {
+app.listen(ṕort, () => {
     console.log('Listening on port 3000...');
 });
