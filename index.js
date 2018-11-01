8eecdfffb2ad2f907f058df496c806e71dbada72
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-31 21:51:07 -0300

    Requires Joi

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+const Joi = require('joi')
 
 app.get('/',  (req, res) => {
    res.send('Hello World');
