57d8e31c1e675536c6a5fbce38892727dc012dcf
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-31 21:57:32 -0300

    Creates object's array

---
+++ store.js
@@ -0,0 +1,32 @@
+let group = [
+    {
+        id: 1,
+        name: 'Leonardo',
+        age: 21,
+        isProgrammer: true
+    },
+    {
+        id: 2,
+        name: 'Joseph',
+        age: 21,
+        isProgrammer: false
+    },
+    {
+        id: 3,
+        name: 'Mariah',
+        age: 24,
+        isProgrammer: true
+    },
+    {
+        id: 4,
+        name: 'Kenny',
+        age: 8,
+        isProgrammer: false
+    },
+    {
+        id: 5,
+        name: 'Maze',
+        age: 39,
+        isProgrammer: true
+    }
+];
