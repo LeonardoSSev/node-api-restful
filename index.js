fb885f06064ea1ccdd238089dce9ddc8fbf2ab1c
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-31 21:58:25 -0300

    Adds missing semicolon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const Joi = require('joi')
+const Joi = require('joi');
 
 let group = require('./store');
 
