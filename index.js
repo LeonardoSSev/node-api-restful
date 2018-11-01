acfc2e0f9dc69adbc24e82cfce6984d906597ff6
Leonardo Santos Severino <leonardossev@gmail.com>
2018-10-31 22:28:56 -0300

    Adds endpoint that changes properties from a stored person and returns the person

--- index.js
+++ index.js
@@ -39,3 +39,15 @@ app.post('/api/group', (req, res) => {
 
     res.status(200).send(person);
 });
+
+app.put('/api/group/:id', (req, res) => {
+    const person = group.find(person => {
+        return person.id === parseInt(req.params.id);
+    });
+
+    person.name = req.body.name;
+    person.age = parseInt(req.body.age);
+    person.isProgrammer = JSON.parse(req.body.isProgrammer);
+
+    res.status(200).send(person);
+});
